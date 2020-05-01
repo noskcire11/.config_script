fe8167b669d7278a483a340752b2eceb52daa7d8
erickson <erickson@noska.com>
2020-05-01 21:15:05 +0700

    add wallpaper

--- script.sh
+++ script.sh
@@ -23,6 +23,10 @@ ln -sfv `pwd`/.script/i3blocks-custom $destination
 ln -sfv `pwd`/.script/i3status $destination
 ln -sfv `pwd`/.script/termite $destination
 
+destination=~/Pictures/
+mkdir -p $destination
+ln -sfv `pwd`/.wallpaper $destination
+
 destination=~/.config/i3blocks-contrib
 read -p 'clone or pull i3blocks-contrib? [y/N] ' input
 if [ $input = 'y' ] || [ $input = 'Y' ]
